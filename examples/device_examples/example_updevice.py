e7803ef0fc5a5f1a5c06e47d2bbb50d23f48d0a1
fendouai <fendouai@gmail.com>
2016-10-09 14:00:31 +0800

    fix the example

--- examples/device_examples/example_updevice.py
+++ examples/device_examples/example_updevice.py
@@ -7,4 +7,3 @@ reg_id = '160a3797c80d93ce897'
 entity = jpush.device_tag(jpush.add("ddd", "tageee"))
 result=device.set_devicemobile(reg_id, entity)
 print (result.status_code)
-print (result.payload)
